6085d8decb51db3a7ce8fa3550acd517d712a220
Evan Varozza <VarozzaEJ@gmail.com>
2025-09-09 09:14:21 -0600

    init

---
+++ client/src/Router.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { createHashRouter } from "react-router-dom";
+import App from "./App.tsx";
+import ErrorPage from "./pages/HomePage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import HelpPage from "./pages/HelpPage.jsx";
+
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "/help",
+        element: <HelpPage />,
+      },
+    ],
+  },
+]);
---
+++ client/src/pages/HelpPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HelpPage() {
+  return <div>This is the help page</div>;
+}
